6a5b884666a8d454501c54e0311fcacbf07bf47b
Finger-cori <badzoo@naver.com>
2025-04-11 17:54:58 +0900

    Update pipe.py

--- src/pipe.py
+++ src/pipe.py
@@ -1,26 +1,25 @@
 from src import NoiseHandler, VoiceEnhancer, AudioFileProcessor, AudioVisualizer
-from src import PyannotDIAR, PyannotVAD
-from src import SBEMB
 from abc import abstractmethod
 from pydub import AudioSegment
 from io import BytesIO
+import tempfile
+from scipy.spatial.distance import cosine
 
 
 class BasePipeline:
     @abstractmethod
     def set_env(self):
-        pass 
+        pass
 
 
-class FrontendProcessor(BasePipeline):
+class FrontendPipe(BasePipeline):
     def set_env(self):
         self.noise_handler = NoiseHandler()
         self.voice_enhancer = VoiceEnhancer()
         self.audio_file_processor = AudioFileProcessor()
 
-    def process_audio(self, audio_file, fade_ms=50, chunk_length=600, deverve=False):
+    def process_audio(self, audio_file, fade_ms=50, chunk_length=300, deverve=False):
         audio_seg = self.audio_file_processor.audiofile_to_AudioSeg(audio_file) 
-        chunk_ms = chunk_length * 1000 
         chunks = self.audio_file_processor.chunk_audio(audio_seg, chunk_length=chunk_length)
         print(f"[DEBUG] Chunk count: {len(chunks)}, chunk_length={chunk_length} sec")
         processed_chunks = []
@@ -34,10 +33,10 @@ class FrontendProcessor(BasePipeline):
                 clean_chunk = self.noise_handler.deverve_audio(denoised)
                 clean_chunk.seek(0)
                 seg = self.audio_file_processor.audiofile_to_AudioSeg(clean_chunk)
-                seg = seg.fade_in(fade_ms).fade_out(fade_ms)
+                # seg = seg.fade_in(fade_ms).fade_out(fade_ms)
             else:
                 seg = self.audio_file_processor.audiofile_to_AudioSeg(denoised)
-                seg = seg.fade_in(fade_ms).fade_out(fade_ms)
+                # seg = seg.fade_in(fade_ms).fade_out(fade_ms)
             processed_chunks.append(seg)
         clean_audio = self.audio_file_processor.concat_chunk(processed_chunks)
         # normalized_audio = self.voice_enhancer.normalize_audio_lufs(clean_audio)
@@ -47,19 +46,7 @@ class FrontendProcessor(BasePipeline):
         self.audio_file_processor.save_audio(audio_file, file_name=file_name)
 
 
-class SpeechActivityDetector(BasePipeline):
-    def set_env(self, vad_config):
-        self.vad_model = PyannotVAD()
-        self.vad_config = vad_config
-        self.audio_visualizer = AudioVisualizer()
-
-    def get_vad_timestamp(self, audio_file):
-        vad_pipeline = self.vad_model.load_pipeline_from_pretrained(self.vad_config)
-        vad_timestamp = self.vad_model.get_vad_timestamp(vad_pipeline, audio_file)
-        return vad_timestamp
-
-
-class PostProcessor(BasePipeline):
+class PostProcessPipe(BasePipeline):
     def set_env(self, emb_config):
         self.audio_file_processor = AudioFileProcessor()
         self.emb_model = SBEMB(emb_config)
