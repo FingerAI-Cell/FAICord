347e6f651689dc98884f6c2f3ca542090b9b004b
Finger-cori <badzoo@naver.com>
2025-04-14 09:24:33 +0900

    Update pipe_test.py
    diar related code delete

--- pipe_test.py
+++ pipe_test.py
@@ -1,4 +1,4 @@
-from src import FrontendPipe, VADPipe, DIARPipe, PostProcessPipe, STTPipe
+from src import FrontendPipe, PostProcessPipe, STTPipe
 from dotenv import load_dotenv
 import numpy as np
 import argparse 
@@ -13,19 +13,16 @@ def main(args):
     whisper_api = os.getenv('WHISPER_API')
     frontend_pipe = FrontendPipe()
     vad_pipe = VADPipe()
-    diar_pipe = DIARPipe()
     stt_pipe = STTPipe()
     
     frontend_pipe.set_env()
     vad_pipe.set_env(os.path.join(args.model_config_path, 'pyannote_vad_config.yaml'))
-    diar_pipe.set_env(os.path.join(args.model_config_path, 'pyannote_diarization_config.yaml'))
     stt_pipe.set_env(whisper_api=whisper_api, generation_config=generation_config)
     
     clean_audio = frontend_pipe.process_audio(args.file_name, chunk_length=300, deverve=True)
     # frontend_pipe.save_audio(clean_audio, 'frontend-processed.wav')
     vad_result = vad_pipe.get_vad_timestamp(clean_audio)
     print(vad_result)
-    diar_result, emb = diar_pipe.get_diar(args.file_name, chunk_length=300, return_embeddings=True, file_name=args.file_name)
     
     
 if __name__ == '__main__':
