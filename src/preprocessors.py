67d32cda9efb61d9e78033368d87c4ba2f8e502e
Finger-cori <badzoo@naver.com>
2025-04-18 16:01:57 +0900

    Update preprocessors.py
    update chunk audio (add time_s, time_e)

--- src/preprocessors.py
+++ src/preprocessors.py
@@ -48,17 +48,24 @@ class AudioFileProcessor:
         else:
             raise TypeError("지원되지 않는 형식입니다: AudioSegment 또는 BytesIO만 지원됩니다.")
 
-    def chunk_audio(self, audio_file, chunk_length=600):
+    def chunk_audio(self, audio_file, chunk_length=600, time_s=None, time_e=None):
         if isinstance(audio_file, BytesIO):
             audio_file.seek(0)
             audio_file = AudioSegment.from_file(audio_file, format="wav")
 
-        if isinstance(audio_file, AudioSegment):
+        if not isinstance(audio_file, AudioSegment):
+            raise TypeError("지원되지 않는 형식입니다: AudioSegment 또는 BytesIO만 지원됩니다.")
+
+        if time_s is not None and time_e is not None:
+            # time_s, time_e는 초 단위로 들어온다고 가정
+            start_ms = int(time_s * 1000)
+            end_ms = int(time_e * 1000)
+            return [audio_file[start_ms:end_ms]]
+        else:
+            # 기존처럼 chunk_length 초 단위로 자르기
             chunk_length_ms = chunk_length * 1000
             chunks = [audio_file[i:i + chunk_length_ms] for i in range(0, len(audio_file), chunk_length_ms)]
             return chunks
-        else:
-            raise TypeError("지원되지 않는 형식입니다: AudioSegment 또는 BytesIO만 지원됩니다.")
 
     def concat_chunk(self, chunk_list):
         return sum(chunk_list)
