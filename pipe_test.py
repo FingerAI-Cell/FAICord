285ad14c35659b71eb572094238b7338f6a4b8c2
Finger-cori <badzoo@naver.com>
2025-04-18 14:09:27 +0900

    Update pipe_test.py
    remove save_audio

--- pipe_test.py
+++ pipe_test.py
@@ -16,7 +16,6 @@ def main(args):
     diar_pipe.set_env(os.path.join(args.model_config_path, 'pyannote_diarization_config.yaml'))
     
     clean_audio = frontend_pipe.process_audio(args.file_name, chunk_length=300, deverve=True)
-    # frontend_pipe.save_audio(clean_audio, 'frontend-processed.wav')
     vad_result = vad_pipe.get_vad_timestamp(clean_audio)
     diar_result, emb_result = diar_pipe.get_diar(args.file_name, chunk_length=300, return_embeddings=True)
     diar_result = diar_pipe.preprocess_result(diar_result=diar_result, vad_result=vad_result, emb_result=emb_result, chunk_offset=300)
