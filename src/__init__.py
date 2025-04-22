e9e85d8c547b95d3fa1c264e7407776c30521acb
Finger-cori <badzoo@naver.com>
2025-04-22 11:04:40 +0900

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,5 @@
 from .preprocessors import DataProcessor, AudioFileProcessor
 from .audio_handler import VoiceEnhancer, NoiseHandler, AudioVisualizer
 from .pyannotes import PyannotVAD, PyannotDIAR, PyannotOSD
-from .speechbrains import SBEMB
-from .wespeaks import WeSPEAKEMB
+from .embeddings import SBEMB, WSEMB
 from .pipe import FrontendPipe, VADPipe, DIARPipe, PostProcessPipe
