7752f682bd2e14c000f4ec745c76af3b1f12e8b8
Finger-cori <badzoo@naver.com>
2025-06-13 15:17:22 +0900

    code update (2025.06.13)

--- pipe_test.py
+++ pipe_test.py
@@ -12,15 +12,12 @@ def main(args):
     Default Setting
     '''
     start = time.time()
+    vad_config = os.path.join(args.model_config_path, 'pyannote_vad_config.yaml')
+    diar_config = os.path.join(args.model_config_path, 'pyannote_diarization_config.yaml')
     frontend_pipe = FrontendPipe()
-    vad_pipe = VADPipe()
-    diar_pipe = DIARPipe()
+    vad_pipe = VADPipe(vad_config)
+    diar_pipe = DIARPipe(diar_config)
     postprocess_pipe = PostProcessPipe()
-    
-    frontend_pipe.set_env()
-    vad_pipe.set_env(os.path.join(args.model_config_path, 'pyannote_vad_config.yaml'))
-    diar_pipe.set_env(os.path.join(args.model_config_path, 'pyannote_diarization_config.yaml'))
-    postprocess_pipe.set_env()
     emb_visualizer = EMBVisualizer()
     '''
     Cleanse audio, Get VAD Result, Get Diar Result, Process Diar Result 
@@ -29,13 +26,18 @@ def main(args):
     vad_result = vad_pipe.get_vad_timestamp(clean_audio)
     diar_result, _ = diar_pipe.get_diar(args.file_name, return_embeddings=False)   # emb 값 사용 x 
     processed_diar, non_overlapped_diar = diar_pipe.preprocess_result(diar_result=diar_result, vad_result=vad_result)    # ok. 
-    relabeled_diar = postprocess_pipe.prepare_nonoverlapped_labels(args.file_name, non_overlapped_diar)
-    final_diar = postprocess_pipe.apply_labels_to_full_diar(processed_diar, relabeled_diar)
-    speaker_emb = postprocess_pipe.get_chunk_emb_array(args.file_name, final_diar)
-    speaker_emb2 = postprocess_pipe.get_chunk_emb_array(args.file_name, diar_result)
-    chunk_idx, emb_array, labels, segments = speaker_emb[2]  # unpack your data
+    # relabeled_diar = postprocess_pipe.relabel_nonoverlapped_labels(args.file_name, non_overlapped_diar)
+    chunk_emb_array = postprocess_pipe.get_chunk_emb_array(args.file_name, non_overlapped_diar)
+    label_mapping_dict = postprocess_pipe.build_label_mapping_dict(chunk_emb_array)
+    print(label_mapping_dict)
+    full_diar = postprocess_pipe.apply_labels_to_full_diar(processed_diar, non_overlapped_diar)
+    # print(full_diar, end='\n\n')
+    # speaker_emb = postprocess_pipe.get_chunk_emb_array(args.file_name, final_diar)
+    # speaker_emb2 = postprocess_pipe.get_chunk_emb_array(args.file_name, diar_result)
+    final_diar = postprocess_pipe.apply_label_mapping_to_diar(full_diar, label_mapping_dict)
+    # print(final_diar)
+    '''chunk_idx, emb_array, labels, segments = speaker_emb[2]  # unpack your data  
     chunk_idx2, emb_array2, labels2, segments2 = speaker_emb2[2]  # unpack your data
-
     emb_visualizer.pca_and_plot(
         embeddings=emb_array,
         labels=labels,
@@ -47,12 +49,12 @@ def main(args):
         labels=labels,
         title=f"Chunk origin {chunk_idx} Speaker Embeddings",
         file_path="."  # 원하는 폴더 경로
-    )
+    )'''
     # print(speaker_emb)
     diar_pipe.save_files(final_diar, file_name=args.file_name)
-    # file_name = args.file_name.split('/')[-1].split('.')[0] 
-    # overlapped_file = args.file_name.replace(file_name, 'non_overlapped_bl_' + file_name)
-    # diar_pipe.save_files(non_overlapped_diar, file_name=overlapped_file)
+    file_name = args.file_name.split('/')[-1].split('.')[0] 
+    overlapped_file = args.file_name.replace(file_name, 'non_overlapped_bl_' + file_name)
+    diar_pipe.save_files(non_overlapped_diar, file_name=overlapped_file)
 
     # overlapped_file = args.file_name.replace(file_name, 'non_overlapped_al_' + file_name)
     # diar_pipe.save_files(relabeled_diar, file_name=overlapped_file)
